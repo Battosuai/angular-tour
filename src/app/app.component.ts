2733d740bc7f610a2133238af544a95ef051deba
Asif <asifur.rahman@vivacomsolutions.com>
2024-06-06 18:13:15 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './UserComponent/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-tour';
+  showUser = false;
+  operatingSystems = [
+    { id: 'win', name: 'Windows' },
+    { id: 'osx', name: 'MacOS' },
+    { id: 'linux', name: 'Linux' },
+  ];
 }
